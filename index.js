f6164fcab464c08f5d92aa7c05f3c40488b16722
Darcy Linde <lindedarcy@gmail.com>
2019-06-29 09:04:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-function findMinAndRemove(array){
-
+function findMinAndRemove(array) {
+  let minValue = array[0];
+  let index = 0;
+  for (let i in array) {
+    if (array[i] < minValue) {
+      minValue = array[i];
+      index = i;
+    }
+  }
+  array.splice(index, 1);
+  return minValue;
 }
 
-function insertionSort(array){
-
+function insertionSort(array) {
+  let newArray = [];
+  let min;
+  while (array.length !== 0) {
+    min = findMinAndRemove(array);
+    newArray.push(min);
+  }
+  return newArray;
 }
